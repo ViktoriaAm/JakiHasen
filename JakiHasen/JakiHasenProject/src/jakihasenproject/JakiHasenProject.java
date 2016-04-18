b54a21d8ec5c58feddb80eb2a14a05f7991286a1
ViktoriaAm <vikidukryte@web.de>
2016-04-18 21:00:36 +0200

    Fertig!

--- JakiHasen/JakiHasenProject/src/jakihasenproject/JakiHasenProject.java
+++ JakiHasen/JakiHasenProject/src/jakihasenproject/JakiHasenProject.java
@@ -7,24 +7,25 @@ package jakihasenproject;
 
 import frameapi.*;
 import java.util.Scanner;
-import javax.swing.JOptionPane;
+//import javax.swing.JOptionPane;
 
 /**
  *
  * @author viktoria.amroudi
  */
-public class JakiHasenProject {
-   
+
+public class JakiHasenProject 
+{
     public static String[][] meinArray = new String [14][14];
     public static void Ausgabe(int [][] meinArray)
     {
         for(int i=0; i<14; i++)
         {
-                 for(int j=0; j<14; j++)
-                 {   
-                   System.out.print(meinArray[i][j]+" ");
-                 }
-                 System.out.println();
+            for(int j=0; j<14; j++)
+            {   
+                System.out.print(meinArray[i][j]+" ");
+            }
+            System.out.println();
         }
         
     }
@@ -35,74 +36,63 @@ public class JakiHasenProject {
         {
             for(int j=0; j<14; j++)
             {
-             meinArray[i][j]=".";
+                meinArray[i][j]=".";
             }
         }
          
         FrameAPI.initFrame();
-        //FrameAPI.initString(meinArray.length, meinArray);
-        //FrameAPI.setPlayground();
     
-        
-          meinArray[0][13]="H";
-          meinArray[11][6]="H";
-          meinArray[5][8]="H";
-          meinArray[12][12]="H";
-          meinArray[5][5]="H";
-          meinArray[7][12]="H";
-          meinArray[1][1]="H";
-          meinArray[7][6]="H";
-          meinArray[2][6]="H";
-          meinArray[3][12]="H";
-          meinArray[1][12]="H";
-          meinArray[7][0]="H";
+        meinArray[0][13]="H";
+        meinArray[11][6]="H";
+        meinArray[5][8]="H";
+        meinArray[12][12]="H";
+        meinArray[5][5]="H";
+        meinArray[7][12]="H";
+        meinArray[1][1]="H";
+        meinArray[7][6]="H";
+        meinArray[2][6]="H";
+        meinArray[3][12]="H";
+        meinArray[1][12]="H";
+        meinArray[7][0]="H";
          
-          meinArray[5][3]="F";
-          meinArray[0][5]="F";
-          meinArray[9][4]="F";
-          meinArray[6][10]="F";
-          meinArray[3][5]="F";
-          meinArray[1][11]="F";
-          meinArray[0][10]="F";
-          meinArray[12][5]="F";
-          meinArray[9][12]="F";
-          meinArray[10][13]="F";
-          meinArray[10][9]="F";
-          meinArray[4][1]="F";
+        meinArray[5][3]="F";
+        meinArray[0][5]="F";
+        meinArray[9][4]="F";
+        meinArray[6][10]="F";
+        meinArray[3][5]="F";
+        meinArray[1][11]="F";
+        meinArray[0][10]="F";
+        meinArray[12][5]="F";
+        meinArray[9][12]="F";
+        meinArray[10][13]="F";
+        meinArray[10][9]="F";
+        meinArray[4][1]="F";
           
-          meinArray[13][3]="J";
-          meinArray[12][2]="J";
-          meinArray[11][1]="J";
-          meinArray[10][0]="J";
-          
-         
+        meinArray[13][3]="J";
+        meinArray[12][2]="J";
+        meinArray[11][1]="J";
+        meinArray[10][0]="J";
           
         FrameAPI.initString(meinArray.length, meinArray);
         FrameAPI.setPlayground();
         
-        
-        
-          do
-          {
-              JOptionPane.showMessageDialog(null, "Neue Runde?");
-              //if(FrameAPI.newRound)
-              {
-              move();
-              FrameAPI.initString(meinArray.length, meinArray);
-              FrameAPI.setPlayground();
-              }
-          }while(!FrameAPI.allDead);
+        do
+        {
+            //JOptionPane.showMessageDialog(null, "Neue Runde?");
+            if(FrameAPI.newRound)
+            {
+                move();
+                FrameAPI.initString(meinArray.length, meinArray);
+                FrameAPI.setPlayground();
+            }
+        }while(!FrameAPI.allDead);
           
-         
-
-          for (int t = 0; t< 10; t++)
-          {           
+        for (int t = 0; t< 10; t++)
+        {           
             System.out.print("Spielfeldausgabe t = " + t);
             System.out.println();
             sc.nextLine();
-          }
-           
-         
+        }    
     }
     public static void move()
     {
@@ -111,7 +101,7 @@ public class JakiHasenProject {
         {
             for (int j=0; j<HilfsArray.length; j++)
             {
-              HilfsArray[i][j] = meinArray[i][j];
+                HilfsArray[i][j] = meinArray[i][j];
             }        
         }
         
@@ -223,7 +213,6 @@ public class JakiHasenProject {
                         meinArray[i][j+1] = "H";
                     }
                 }
-                
             }
         }
                 
@@ -286,8 +275,6 @@ public class JakiHasenProject {
                         }
                     }
                 }
-                
-              
             }
         }
         
@@ -297,7 +284,6 @@ public class JakiHasenProject {
             {
                 if(HilfsArray[i][j].equals("J"))
                 {
-                    
                     if(j==13)
                     {
                         meinArray[i][13] = ".";
